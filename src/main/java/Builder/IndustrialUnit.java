92a1bbb343566a47853d6c98af49f8456bab9b3f
megabart <megabart57@gmail.com>
2022-07-10 15:25:27 +0300

    Builder

---
+++ src/main/java/Builder/IndustrialUnit.java
@@ -0,0 +1,47 @@
+package Builder;
+
+public class IndustrialUnit {
+
+private String Name;
+private float Temperature;
+private double Pressure;
+private int Voltage;
+private String SupplierName;
+private OpratingStatus opratingStatus;
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setTemperature(float temperature) {
+        Temperature = temperature;
+    }
+
+    public void setPressure(double pressure) {
+        Pressure = pressure;
+    }
+
+    public void setVoltage(int voltage) {
+        Voltage = voltage;
+    }
+
+    public void setSupplierName(String supplierName) {
+        SupplierName = supplierName;
+    }
+
+    public void setOpratingStatus(OpratingStatus opratingStatus) {
+        this.opratingStatus = opratingStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "IndustrialUnit{" +
+                "Name='" + Name + '\'' +
+                ", Temperature=" + Temperature +
+                ", Pressure=" + Pressure +
+                ", Voltage=" + Voltage +
+                ", SupplierName='" + SupplierName + '\'' +
+                ", opratingStatus=" + opratingStatus +
+                '}';
+    }
+}
---
+++ src/main/java/Builder/OpratingStatus.java
@@ -0,0 +1,7 @@
+package Builder;
+
+public enum OpratingStatus {
+
+  OPERATING,
+  STOP;
+}
